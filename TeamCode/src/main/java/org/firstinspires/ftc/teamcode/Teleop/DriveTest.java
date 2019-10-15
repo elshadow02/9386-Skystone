d92b1af621dbfdfd24ca787cc8a9411c813a280e
elshadow02 <elshadow0202@gmail.com>
2019-10-15 15:44:58 -0500

    Adding dampning and fixed driveTest.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Teleop/DriveTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Teleop/DriveTest.java
@@ -32,22 +32,30 @@ public class DriveTest extends OpMode{
     @Override
     public void loop() {
 
-        Orientation orientation = robot.imu.getAngularOrientation(AxesReference.EXTRINSIC, AxesOrder.XYZ, AngleUnit.DEGREES);
+        Orientation orientation = robot.imu.getAngularOrientation(AxesReference.EXTRINSIC, AxesOrder.XYZ, AngleUnit.RADIANS);
 
         //Assigns joystickAngle as the angle made by the left joystick.
         joystickAngle = Math.atan2(-gamepad1.left_stick_y, gamepad1.left_stick_x);
-
-        joystickAngle = Math.toDegrees(joystickAngle);
 
         //Defines angleChange variable as the x-value received from the imu.
         angleChange = orientation.thirdAngle;
         //Assigns speedAngle as the difference between joystickAngle and angleChange.
         speedAngle = joystickAngle - angleChange;
 
-        speedAngle = Math.toRadians(speedAngle);
-
         //Gets the rotational value for our drive.
         rotate = kR * gamepad1.right_stick_x;
+
+        double dampner;
+
+        if(Math.abs(gamepad1.left_stick_x) > Math.abs(gamepad1.left_stick_y)){
+            dampner = Math.abs(gamepad1.left_stick_x);
+        }
+        else if (Math.abs(gamepad1.left_stick_y) > Math.abs(gamepad1.left_stick_x)){
+            dampner = Math.abs(gamepad1.left_stick_y);
+        }
+        else{
+            dampner = Math.abs(gamepad1.left_stick_y);
+        }
 
         //Make sure that the absolute value of either the y- or x-value of the joystick is greater than 0.1.
         //Because speedAngle will never produce an angle where the sine and cosine of that angle equals 0,
@@ -74,10 +82,10 @@ public class DriveTest extends OpMode{
                 backRightSpeed /= max;
             }
 
-            robot.frontRight.setPower(frontRightSpeed);
-            robot.frontLeft.setPower(frontLeftSpeed);
-            robot.backLeft.setPower(backLeftSpeed);
-            robot.backRight.setPower(backRightSpeed);
+            robot.frontRight.setPower(frontRightSpeed * dampner);
+            robot.frontLeft.setPower(frontLeftSpeed * dampner);
+            robot.backLeft.setPower(backLeftSpeed * dampner);
+            robot.backRight.setPower(backRightSpeed * dampner);
         }
         else{
             robot.frontRight.setPower(-rotate);
